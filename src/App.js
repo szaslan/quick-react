101acf2bfdae7fec363916c884ba9fd2395fbd06
Zack <szaslan@gmail.com>
2020-01-09 20:37:51 -0600

    first slice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const schedule = {
+  title: "CS Courses for 2018-2019"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>{schedule.title}</h1>
+  </div>
+);
 
 export default App;
