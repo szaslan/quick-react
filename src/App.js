f89eb39f88f71d96923ccd50ab90286f50e9d8fb
Zack <szaslan@gmail.com>
2020-01-09 20:44:28 -0600

    slice four

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "rbx/index.css";
 import { Button, Container, Title } from "rbx";
 const schedule = {
@@ -48,10 +49,25 @@ const CourseList = ({ courses }) => (
 );
 
 const Banner = ({ title }) => <Title>{title}</Title>;
-const App = () => (
-  <Container>
-    <Banner title={schedule.title} />
-    <CourseList courses={schedule.courses} />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: "", courses: [] });
+  const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    };
+    fetchSchedule();
+  }, []);
+
+  return (
+    <Container>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </Container>
+  );
+};
 export default App;
